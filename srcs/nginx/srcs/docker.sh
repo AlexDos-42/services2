2d3321cb1213d2e05e6bf96309fa6ea638896cb7
Alex 42 <92.alex.ds@gmail.com>
2020-08-22 10:35:39 +0200

    Add files via upload

--- srcs/nginx/srcs/docker.sh
+++ srcs/nginx/srcs/docker.sh
@@ -1,11 +1,1 @@
-[ -z "$SSH_USER" ] && SSH_USER=alesanto
-[ -z "$SSH_PASSWORD" ] && SSH_PASSWORD=admin
-
-adduser -D "$SSH_USER"
-echo "$SSH_USER:$SSH_PASSWORD" | chpasswd
-echo "user:password = $SSH_USER:$SSH_PASSWORD"
-
-
-nginx
-/usr/bin/ssh-keygen -A
-/usr/sbin/sshd
+nginx -g 'daemon off;'
